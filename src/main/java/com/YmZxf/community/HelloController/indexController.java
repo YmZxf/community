0cc7cf76bd4bf18ac8641327268f22119ac76435
YmZxf <1455895416@qq.com>
2020-07-21 19:17:44 +0800

    7-21 02

--- src/main/java/com/YmZxf/community/HelloController/indexController.java
+++ src/main/java/com/YmZxf/community/HelloController/indexController.java
@@ -1,16 +1,34 @@
 package com.YmZxf.community.HelloController;
 
+import com.YmZxf.community.mapper.UserMapper;
+import com.YmZxf.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
 public class indexController {
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/")
-    public String hello(@RequestParam(name="name",required = true,defaultValue = "Zheng") String name, Model model){
-        model.addAttribute("name",name);
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie:cookies) {
+            if("token".equals(cookie.getName())){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+
+
         return "index";
     }
 }
--- src/main/java/com/YmZxf/community/mapper/UserMapper.java
+++ src/main/java/com/YmZxf/community/mapper/UserMapper.java
@@ -3,9 +3,14 @@ package com.YmZxf.community.mapper;
 import com.YmZxf.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into tb_user values(#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified})")
     public void insert(User user);
+    @Select("select * from tb_user where token=#{token}")
+    User findByToken(@Param("token") String token);
+
 }
